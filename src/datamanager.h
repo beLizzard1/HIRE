679c32a06a32d4f18326e5afb63a384dd7f3b982
vivian528 <vivian528@ubuntu.(none)>
2013-07-01 12:00:01 +0100

    deleted:    datamanager.c 	deleted:    datamanager.h 	deleted:    fftcuda.cu 	modified:   main.c 	modified:   makefile

--- src/datamanager.h
+++
@@ -1,5 +0,0 @@
-int createdoublematrix(double ***mat, unsigned short int rows, unsigned short int cols);
-
-int freedoublematrix(double **mat, unsigned int cols);
-
-int splittingdoublematrix(double **mat, double ***image1, double ***image2);
--- src/main.c
+++ src/main.c
@@ -1,49 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <math.h>
-#include <cuda.h>
-#include <string.h>
-#include "datamanager.h"
-#include <time.h>
+#include <tiffio.h>
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
 
-	if( (argc != 2) ){
+	argc = argc;
+	int error = 0;
+	uint16 bps;
+	uint32 width, height, array, length;
+	TIFF *image;
+		
+	if((image = TIFFOpen(argv[1],"r")) == NULL){
 
-		printf("Please provide the correct operating parameters\n");
-		printf("Run the program with the image file you require\n");
-		printf("to be reconstructed\n");
-		return(1);
+		printf("Error opening the TIFF Image\n");
 	}
 
-	printf("This is an implementation of the MATLAB holographic reconstruction program in C\n");
-	printf("Later it will be enhanced with CUDA calls to improve performance\n");
+	TIFFGetField(image, TIFFTAG_BITSPERSAMPLE, &bps);
+	TIFFGetField(image, TIFFTAG_IMAGEWIDTH, &width);
+	TIFFGetField(image, TIFFTAG_IMAGELENGTH, &length);
+
+	printf("Bits Per Sample: %"PRId16", Image Width: %"PRId32", Image Length/Height: %"PRId32"\n", bps, width,length);  
+
+	height = length;
+
+	while(error != 0){
+		TIFFReadRGBAImage(image, width, height, &array, error);
+	}
 
 
-	argv=argv;
-	
-	
-	double** array;
-	unsigned short int width, height;
-	width = 1392;
-	height = 2080;
-
-	createdoublematrix(&array,width,height);
+	TIFFClose(image);
 
 
-	/* So we have a big 2d matrix allocated in the Host memory we want to split this matrix in half so we can deal with the two images contained within the array */
+return(0);
 
-	double **image1, **image2;
-
-	splittingdoublematrix(array,&image1,&image2);
-
-	freedoublematrix(array,height);
-
-	/* Now we have the two separate arrays loaded into the Host memory */
-
-	image1 = image1;
-	image2 = image2;
-
-
-return(0);	
 }
