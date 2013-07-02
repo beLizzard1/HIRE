f79f50d6d3fd9e852ad3472f390efb1b7aa2066f
vivian528 <vivian528@ubuntu.(none)>
2013-07-02 10:33:15 +0100

    Trying the scanline technique 	modified:   main.c

--- src/main.c
+++ src/main.c
@@ -13,12 +13,12 @@
 #define grayscale(v) ((TIFFGetR(v)+TIFFGetG(v)+TIFFGetB(v))/3)
 
 int main(int argc, char *argv[]){
-	
+	short config;
 	argc = argc;
 	uint16 bps,spp;
-	uint32 width, height, length, *array;
+	uint32 width, height, length, currow;
 	TIFF *image;
-	
+	tdata_t buffer;
 	
 	if((image = TIFFOpen(argv[1],"r")) == NULL){
 		printf("Error opening the TIFF Image\n");
@@ -29,40 +29,29 @@ int main(int argc, char *argv[]){
 	TIFFGetField(image, TIFFTAG_BITSPERSAMPLE, &bps);
 	TIFFGetField(image, TIFFTAG_IMAGEWIDTH, &width);
 	TIFFGetField(image, TIFFTAG_IMAGELENGTH, &length);
-	TIFFGetField(image, TIFFTAG_SAMPLESPERPIXEL, &spp);	
+	TIFFGetField(image, TIFFTAG_SAMPLESPERPIXEL, &spp);
+	TIFFGetField(image, TIFFTAG_PLANARCONFIG, &config);
 
-	printf("Bits Per Sample: %"PRId16", Samples Per Pixel: %"PRId16", Image Width: %"PRId32", Image Length/Height: %"PRId32"\n", bps,spp,width,length);  
+
+	printf("Planar Config: %d Bits Per Sample: %"PRId16", Samples Per Pixel: %"PRId16", Image Width: %"PRId32", Image Length/Height: %"PRId32"\n", config, bps,spp,width,length);  
 
 	height = length;
 
-	printf("\nWe now going to load the entire image into a raster \n");	
-
-	array = (uint32 *)_TIFFmalloc((width * height) * sizeof(uint32));
-
-	TIFFReadRGBAImage(image, width, height, array, 0);
-
-	// Now need to be smart about how we read the image, raster images start at the bottom left.
-
-	unsigned int x,y,arrayint;
-	uint32 data;
-
-	for(x = 0; x < width; x++){
-
-		for(y = 0; y < height; y++){
-
-			arrayint = coordtoraster(width, x, y);
-			data = array[arrayint];
-			printf("%d\n",grayscale(data));			
-//			printf("(%d,%d) Value: %d\n",x,y,grayscale(data));	
-
-		}
-	}
+	buffer = _TIFFmalloc(TIFFScanlineSize(image));
 	
-		
-	//Freeing the allocated memory & Closing stuff
-	TIFFClose(image);
+	for (currow = 0; currow < height; currow ++ ){
+		printf("Row: %"PRId32"\n", currow);
+		if(TIFFReadScanline(image,buffer,currow,0) == -1){
+			printf("An error occured when processing the image\n");
+			return(1);
+		}
 
-	_TIFFfree(array);
+	}
+
+
+
+	_TIFFfree(buffer);
+	TIFFClose(image);
 
 return(0);
 
