5d8d79d7968b0fd05a86a5e0d0c8982b053a3326
vivian528 <vivian528@ubuntu>
2013-07-05 12:08:52 +0100

    new file:   cufft.cu 	new file:   cufft.cu.h 	modified:   main.c 	modified:   makefile 	new file:   matrix.c 	new file:   matrix.h 	deleted:    rasterarray.c 	deleted:    rasterarray.h
    Okay reading tiffs(good)

--- src/main.c
+++ src/main.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 #include <inttypes.h>
 #include <math.h>
 #include <tiffio.h>
-#include "rasterarray.h"
-
-#define TIFFGetR(abgr) ((abgr) & 0xff)
-#define TIFFGetG(abgr) (((abgr) >> 8) & 0xff)
-#define TIFFGetB(abgr) (((abgr) >> 16) & 0xff)
-#define TIFFGetA(abgr) (((abgr) >> 24) & 0xff)
-#define grayscale(v) ((TIFFGetR(v)+TIFFGetG(v)+TIFFGetB(v))/3)
+#include <cuda.h>
+#include "cufft.cu.h"
+#include "matrix.h"
 
 int main(int argc, char *argv[]){
 	short config;
 	argc = argc;
-	uint16 bps,spp;
-	uint32 width, height, length, currow;
+	uint16 bps,spp, *buffer;
+	uint32 width, length, currow, totalpixel;
 	TIFF *image;
-	tdata_t buffer;
 	
+
 	if((image = TIFFOpen(argv[1],"r")) == NULL){
 		printf("Error opening the TIFF Image\n");
 		return(1);
@@ -32,27 +27,87 @@ int main(int argc, char *argv[]){
 	TIFFGetField(image, TIFFTAG_SAMPLESPERPIXEL, &spp);
 	TIFFGetField(image, TIFFTAG_PLANARCONFIG, &config);
 
+	printf("Planar Config: %d, Bits Per Sample: %"PRId16", Samples Per Pixel: %"PRId16", Image Width: %"PRId32", Image Length/Height: %"PRId32"\n", config, bps,spp,width,length);  
 
-	printf("Planar Config: %d Bits Per Sample: %"PRId16", Samples Per Pixel: %"PRId16", Image Width: %"PRId32", Image Length/Height: %"PRId32"\n", config, bps,spp,width,length);  
+	totalpixel = length * width;
 
-	height = length;
+	unsigned int i;
+	uint16 **image1, **image2;
+	
+	creatematrix(&image1, width, length);
+	creatematrix(&image2, width, length);
 
 	buffer = _TIFFmalloc(TIFFScanlineSize(image));
-	
-	for (currow = 0; currow < height; currow ++ ){
-		printf("Row: %"PRId32"\n", currow);
-		if(TIFFReadScanline(image,buffer,currow,0) == -1){
-			printf("An error occured when processing the image\n");
-			return(1);
-		}
 
+	printf("Created the blocks in the host memory\n");
+        
+	for (currow = 0; currow < 1039; currow ++ ){
+                if(TIFFReadScanline(image,buffer,currow,0) == -1){
+                        printf("An error occured when processing the image\n");
+                        return(1);
+                }
+                for(i = 0; i < width; i++){
+                       image1[i][currow] = buffer[i];
+                }
+        }
+
+	uint32 row;	
+
+        for (currow = 1040; currow < length; currow++){
+                if(TIFFReadScanline(image,buffer,currow,0) == -1){
+                        printf("An error occured when processing the image\n");
+                        return(1);
+                }
+                
+                for(i = 0; i < width; i++){
+			row = currow - 1040;
+			image2[i][row] = buffer[i];
+                }
+        }
+
+
+	// Finishing loading the image ( & splitting it ), now have two arrays of unsigned 16 bit integers to play around with. 
+
+	// Cleaning up stuff that isn't needed any more
+	
+	_TIFFfree(buffer);
+	TIFFClose(image);
+		
+	// Now we need to subtract the two images to remove the noise and prepare the resulting array/matrix for the result.
+	short int **matrix;
+	unsigned int x,y;
+
+	matrix = (int16 **)calloc(width, sizeof(int16 *));
+	for(i=0; i < width; i++){
+		matrix[i] = (int16 *)calloc(length,sizeof(int16));
 	}
 
 
+	for(x = 0; x < width; x++){
+		for(y = 1; y < length; y++){
+			matrix[x][y] = (image2[x][y] - image1[x][y]);
+			// printf("Intensity: %+d\n", matrix[x][y]);
+			}
+	}
+	
+	//Cleaning up the two images
+	freematrix(image1,width);
+	freematrix(image2,width);
 
-	_TIFFfree(buffer);
-	TIFFClose(image);
+
+	//Move to GPU
+	printf("Starting GPU based stuff\n");
+	gpucalculation(width, length/2, matrix);
+
+	
+	printf("Finished \n");
+
+	//Cleaning up the subtracted image
+	for(i=0; i < width; i++){
+		free(matrix[i]);
+	}
+	free(matrix);
+
 
 return(0);
-
 }
--- src/rasterarray.h
+++
@@ -1,1 +0,0 @@
-int coordtoraster(unsigned int width, unsigned int x, unsigned int y);
